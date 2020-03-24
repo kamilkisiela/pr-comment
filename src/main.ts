0b623670434fc5ad93b0bbb159b7a40c016dc267
Kamil Kisiela <kamil.kisiela@gmail.com>
2020-03-24 19:26:08 +0100

    Init

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,92 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import axios from 'axios'
+
+const [owner, repo] = process.env.GITHUB_REPOSITORY!.split('/')
+const url = `https://api.github.com/repos/${owner}/${repo}`
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    core.debug('Running PR Comment Action...')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const headers = {
+      Authorization: `Bearer ${core.getInput('bot-token')}`
+    }
+    const prId = getPullRequestID(process.env.GITHUB_REF!)
+    const api = createAPI(headers)
+    const existingId = await api.find(prId, core.getInput('bot'))
 
-    core.setOutput('time', new Date().toTimeString())
+    const message = core.getInput('message')
+
+    if (existingId) {
+      await api.update(existingId, message)
+      core.info('Updated a comment')
+    } else {
+      await api.create(prId, message)
+      core.info('Created a new comment')
+    }
   } catch (error) {
     core.setFailed(error.message)
   }
+}
+
+function createAPI(headers: Record<string, string>) {
+  async function find(prId: string, botName: string) {
+    const {data} = await axios.get(`${url}/issues/${prId}/comments`, {
+      responseType: 'json',
+      headers
+    })
+    const comments = data
+
+    if (comments && Array.isArray(comments)) {
+      const found = comments.find(comment => comment.user.login === botName)
+
+      if (found) {
+        return found.id
+      }
+    }
+  }
+
+  async function create(prId: string, body: string) {
+    await axios.post(
+      `${url}/issues/${prId}/comments`,
+      {body},
+      {
+        responseType: 'json',
+        headers
+      }
+    )
+  }
+
+  /**
+   *
+   * @param {string} commentId
+   */
+  async function update(id: string, body: string) {
+    await axios.patch(
+      `${url}/issues/comments/${id}`,
+      {body},
+      {
+        responseType: 'json',
+        headers
+      }
+    )
+  }
+
+  return {
+    update,
+    create,
+    find
+  }
+}
+
+function getPullRequestID(ref: string) {
+  const result = /refs\/pull\/(\d+)\/merge/g.exec(ref)
+
+  if (!result) {
+    throw new Error('Github reference not found.')
+  }
+  const [, pullRequestId] = result
+  return pullRequestId
 }
 
 run()
